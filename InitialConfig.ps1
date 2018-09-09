d958cc5ba2e4d4e43254b2b6a5ad872d5d6d1771
nyanhp <nyanhp@users.noreply.github.com>
2018-09-09 13:59:16 +0200

    Library drive letter changed

--- InitialConfig.ps1
+++ InitialConfig.ps1
@@ -24,7 +24,7 @@ configuration CloudGamingClient
     Disk dataDisk
     {
         DiskId      = 2
-        DriveLetter = 'D'
+        DriveLetter = 'L'
         #DependsOn   = '[Disk]tempDisk'
         DiskIdType  = 'Number'
         FSFormat    = 'NTFS'
